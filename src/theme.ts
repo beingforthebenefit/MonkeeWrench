59b6e4f850bea8f7f8b18e1e96c726a34594f37b
Gerald Todd <beingforthebenefit@gmail.com>
2025-08-30 09:02:51 -0700

    feat: add dark theme configuration using MUI

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: { main: "#B71C1C" },
+    background: { default: "#0b0b0b", paper: "#111" }
+  }
+});
