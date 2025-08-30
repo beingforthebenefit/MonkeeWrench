0f2e3c1656c1a9e5013e069c36287428ef069037
Gerald Todd <beingforthebenefit@gmail.com>
2025-08-30 09:06:44 -0700

    feat: add Providers component for session and theme management

---
+++ src/components/Providers.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { ReactNode } from 'react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { theme } from '@/theme'
+import { SessionProvider } from 'next-auth/react'
+
+export default function Providers({
+  children,
+  session,
+}: {
+  children: ReactNode
+  session: any
+}) {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  )
+}
