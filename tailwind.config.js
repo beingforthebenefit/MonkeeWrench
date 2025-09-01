cee0ab8393b34cd103c963234de3a027f7b41a80
Gerald Todd <beingforthebenefit@gmail.com>
2025-09-01 10:48:40 -0700

    Add tailwind and postcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        monkee: { red: "#B71C1C", dark: "#111113" }
+      }
+    }
+  },
+  plugins: [], // <- no @tailwindcss/forms on v4
+};
